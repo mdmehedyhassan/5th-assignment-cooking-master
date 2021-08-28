e58d62cdf193d79830cffd6fc71c9a5cc4ae1878
mdmehedyhassan <mdmehedyh55@gmail.com>
2021-08-28 23:28:22 +0600

    add not match, not set, and style

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
-function searchButtonHandler(){
+function searchButtonHandler() {
+    document.getElementById('male-item-notSetup').innerHTML = null;
+    document.getElementById('male-item-notMatch').innerHTML = null;
+    document.getElementById('male-item').innerHTML = null;
     const searchInput = document.getElementById('searchInput').value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`)
-    .then(response => response.json())
-    .then(data => searchMale(data.meals))
-    .catch(error => console.log(error))
+    if (searchInput == '') {
+        document.getElementById('male-item-notSetup').innerHTML = '<h1 class="text-danger text-center">Sorry!!! You are searching without typing anything.</h1>'
+    }
+    else {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`)
+            .then(response => response.json())
+            .then(data => searchMale(data.meals))
+            .catch(error => {
+                document.getElementById('male-item-notMatch').innerHTML = `<h1 class="text-danger text-center">Sorry!!! <span class="text-info">${searchInput}</span> this meal does not match. Please search again.</h1>`;
+            })
+    }
 }
 
-function searchMale(males){
+function searchMale(males) {
     males.forEach(male => {
-        const maleMainDiv = document.getElementById('male-item')
-        const addNewDiv = document.createElement('div')
-        addNewDiv.classList.add('col-md-4')
-        addNewDiv.classList.add('col-sm-6')
+        const maleMainDiv = document.getElementById('male-item');
+        const addNewDiv = document.createElement('div');
+        addNewDiv.classList.add('col-md-4');
+        addNewDiv.classList.add('col-sm-6');
         addNewDiv.innerHTML = `
             <div class="male-item-style">
                 <div class="image-div">
@@ -21,7 +31,6 @@ function searchMale(males){
             </div>
         `
         maleMainDiv.appendChild(addNewDiv)
-        console.log(male.strMeal)
     })
 }
 
